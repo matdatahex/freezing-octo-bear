47a0a8a6265c9b06ea68fbddd5c1cea787a0936e
vagrant <vagrant@precise32.(none)>
2013-04-19 13:47:39 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreezingOctoBear::Application.config.secret_token = '6a02d54b33268fee39e70e722e3b4725d0dae9bcb4d06a090243f8b622770213a461cbcef6eca3e7e5e9d2845acc14d874c0f8cb45dcf3e3146f7d9845d37d22'
