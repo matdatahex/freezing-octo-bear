bb7c42fa9e882f6aebdaa4d5ebc80a4e8d7daff2
Mathieu Levesque-Lavallee <mathieu.levesque-lavallee@datahex.ca>
2013-04-19 09:45:20 -0400

    Delete all

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FreezingOctoBear::Application.config.secret_token = '89c6864b2382cfbf34f309479f0ad310b7ea013e4ea16fc89f6b2a062854157d4f496de14298a811f7f745964877225b703abbe3903625c5764debeee6b377b7'
